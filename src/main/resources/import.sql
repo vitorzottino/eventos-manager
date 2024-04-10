10e90f363f08d6cc4826269ed735c9b547d19b53
Enzo Pavanello <112507049+Enzo-Pavanello@users.noreply.github.com>
2024-04-10 10:25:35 -0300

    Add files via upload

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_cidade(nome) VALUES('São Paulo');
+INSERT INTO tb_cidade(nome) VALUES('Rio de Janeiro');
+INSERT INTO tb_cidade(nome) VALUES('Belo Horizonte');
+INSERT INTO tb_cidade(nome) VALUES('Salvador');
+INSERT INTO tb_cidade(nome) VALUES('Santa Catarina');
+INSERT INTO tb_cidade(nome) VALUES('Blumenau');
+
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Só Track Boa', '14/06/2024', 'https://www.sotrackboa.com.br/', 5);
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Rock In Rio', '21/06/2024', 'https://rockinrio.com/rio/pt-br/home/', 1);
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Lollapalooza', '01/08/2024', 'https://www.lollapaloozabr.com/', 2);
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Oktoberfest', '21/09/2024', 'https://oktoberfestblumenau.com.br/', 3);
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Formula 1', '03/11/2024', 'https://www.formula1.com/', 4);
